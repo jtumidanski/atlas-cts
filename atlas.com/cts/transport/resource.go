f7d4221eaa39907415d07372ab13b0c3923cca90
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-10-14 13:54:27 -0400

    All REST handlers consider OpenTracing span context.

--- atlas.com/cts/transport/resource.go
+++ atlas.com/cts/transport/resource.go
@@ -3,87 +3,109 @@ package transport
 import (
 	"atlas-cts/configuration"
 	"atlas-cts/json"
+	"atlas-cts/rest"
 	"github.com/gorilla/mux"
+	"github.com/opentracing/opentracing-go"
 	"github.com/sirupsen/logrus"
 	"net/http"
 	"strconv"
 )
 
+const (
+	GetTransport  = "get_transport"
+	GetTransports = "get_transports"
+)
 
 func InitResource(router *mux.Router, l logrus.FieldLogger, c *configuration.Configuration) {
 	tr := router.PathPrefix("/transports").Subrouter()
-	tr.HandleFunc("/", HandleGetTransport(l, c)).Queries("source", "{source}", "destination", "{destination}").Methods(http.MethodGet)
-	tr.HandleFunc("/", HandleGetTransports(l, c)).Methods(http.MethodGet)
+	tr.HandleFunc("/", registerGetTransport(l, c)).Queries("source", "{source}", "destination", "{destination}").Methods(http.MethodGet)
+	tr.HandleFunc("/", registerGetTransports(l, c)).Methods(http.MethodGet)
 }
 
-func HandleGetTransports(l logrus.FieldLogger, c *configuration.Configuration) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		transports, err := GetAll(l, c)()
-		if err != nil {
-			l.WithError(err).Errorf("Unable to get transports configured for service.")
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
+func registerGetTransports(l logrus.FieldLogger, c *configuration.Configuration) http.HandlerFunc {
+	return rest.RetrieveSpan(GetTransports, func(span opentracing.Span) http.HandlerFunc {
+		return handleGetTransports(l, c)(span)
+	})
+}
 
-		response := &dataListContainer{}
-		for _, t := range transports {
-			response.Data = append(response.Data, makeDataBody(t))
-		}
+func handleGetTransports(l logrus.FieldLogger, c *configuration.Configuration) func(span opentracing.Span) http.HandlerFunc {
+	return func(span opentracing.Span) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			transports, err := GetAll(l, c)()
+			if err != nil {
+				l.WithError(err).Errorf("Unable to get transports configured for service.")
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
 
-		err = json.ToJSON(response, w)
-		if err != nil {
-			l.WithError(err).Errorf("Writing output")
-			w.WriteHeader(http.StatusInternalServerError)
+			response := &dataListContainer{}
+			for _, t := range transports {
+				response.Data = append(response.Data, makeDataBody(t))
+			}
+
+			err = json.ToJSON(response, w)
+			if err != nil {
+				l.WithError(err).Errorf("Writing output")
+				w.WriteHeader(http.StatusInternalServerError)
+			}
 		}
 	}
 }
 
-func HandleGetTransport(l logrus.FieldLogger, c *configuration.Configuration) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		filters := make([]Filter, 0)
-		if val, ok := mux.Vars(r)["source"]; ok {
-			source, err := strconv.Atoi(val)
+func registerGetTransport(l logrus.FieldLogger, c *configuration.Configuration) http.HandlerFunc {
+	return rest.RetrieveSpan(GetTransport, func(span opentracing.Span) http.HandlerFunc {
+		return handleGetTransport(l, c)(span)
+	})
+}
+
+func handleGetTransport(l logrus.FieldLogger, c *configuration.Configuration) func(span opentracing.Span) http.HandlerFunc {
+	return func(span opentracing.Span) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			filters := make([]Filter, 0)
+			if val, ok := mux.Vars(r)["source"]; ok {
+				source, err := strconv.Atoi(val)
+				if err != nil {
+					l.WithError(err).Errorf("Unable to properly parse source from path.")
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+				filters = append(filters, MatchSource(uint32(source)))
+			}
+			if val, ok := mux.Vars(r)["destination"]; ok {
+				destination, err := strconv.Atoi(val)
+				if err != nil {
+					l.WithError(err).Errorf("Unable to properly parse destination from path.")
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+				filters = append(filters, MatchDestination(uint32(destination)))
+			}
+
+			transports, err := GetFiltered(l, c)(filters...)()
 			if err != nil {
-				l.WithError(err).Errorf("Unable to properly parse source from path.")
-				w.WriteHeader(http.StatusBadRequest)
+				l.WithError(err).Errorf("Unable to get transports configured for service.")
+				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
-			filters = append(filters, MatchSource(uint32(source)))
-		}
-		if val, ok := mux.Vars(r)["destination"]; ok {
-			destination, err := strconv.Atoi(val)
-			if err != nil {
-				l.WithError(err).Errorf("Unable to properly parse destination from path.")
-				w.WriteHeader(http.StatusBadRequest)
-				return
+
+			response := &dataListContainer{}
+			for _, t := range transports {
+				response.Data = append(response.Data, makeDataBody(t))
 			}
-			filters = append(filters, MatchDestination(uint32(destination)))
-		}
 
-		transports, err := GetFiltered(l, c)(filters...)()
-		if err != nil {
-			l.WithError(err).Errorf("Unable to get transports configured for service.")
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		response := &dataListContainer{}
-		for _, t := range transports {
-			response.Data = append(response.Data, makeDataBody(t))
-		}
-
-		err = json.ToJSON(response, w)
-		if err != nil {
-			l.WithError(err).Errorf("Writing output")
-			w.WriteHeader(http.StatusInternalServerError)
+			err = json.ToJSON(response, w)
+			if err != nil {
+				l.WithError(err).Errorf("Writing output")
+				w.WriteHeader(http.StatusInternalServerError)
+			}
 		}
 	}
 }
 
 func makeDataBody(t *Model) dataBody {
 	return dataBody{
-		Id:         "",
-		Type:       "",
+		Id:   "",
+		Type: "",
 		Attributes: attributes{
 			Enabled:     t.Enabled(),
 			Source:      t.Source(),
